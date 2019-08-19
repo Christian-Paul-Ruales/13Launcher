2e990615dd13c5d8244bf363a0e5839e522e8457
Christian-Paul-Ruales <christianpaulruales@gmail.com>
2019-08-18 22:41:00 -0500

    beta1.2

--- app/src/main/java/com/lr_apps/rj/a13launcher/AppsList_Activity.kt
+++ app/src/main/java/com/lr_apps/rj/a13launcher/AppsList_Activity.kt
@@ -5,19 +5,17 @@ import android.content.pm.PackageManager
 import android.content.pm.ResolveInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.annotation.NonNull
-import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.RecyclerView
+import android.support.v4.content.ContextCompat.startActivity
 import android.view.View
 import android.widget.*
-import android.support.v7.widget.LinearLayoutManager
-
+import java.util.jar.Attributes
 
 
 class AppsList_Activity : AppCompatActivity() {
     private var manager:PackageManager?=null /**Manejar las apps*/
     private var Apps:ArrayList<Item>?=null /**Lista de apps*/
     private var AppsListView:GridView?=null /**Listview para mostrar las apps(grafico)*/
+    private var searchAppsView:SearchView?=null
 
     //Metodo Creacion, Similar a un constructor
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +24,7 @@ class AppsList_Activity : AppCompatActivity() {
         LoadApps()
         LoadListView()
         onClickListener()
+        OntextChange()
     }
 
     /**----------------------------Metodo Cargar Apps -----------------------*/
@@ -38,26 +37,110 @@ class AppsList_Activity : AppCompatActivity() {
         intent.addCategory(Intent.CATEGORY_LAUNCHER)
 
         var availableActivities:List<ResolveInfo> = manager!!.queryIntentActivities(intent,0) /**Toma apps del telefono*/
+        val sortedAppsList = availableActivities.sortedBy { it.loadLabel(manager)?.toString() }
+        availableActivities = sortedAppsList
 
-    /**-------------------------Guardar Datos de las apps en una lista-----------------*/
+        /**-------------------------Guardar Datos de las apps en una lista-----------------*/
         for (r1 in availableActivities){
             var app:Item = Item()
             app!!.label=r1.activityInfo.packageName
-            print("++++++++++++++++++++++++++++++++app.label:"+app!!.label+"\n\n\n")
-            print("+-------------------------r1.activityinfo.pakagename: "+r1.activityInfo.packageName+"\n\n\n\n")
 
             app!!.name=r1.loadLabel(manager)
-            print("++++++++++++++++++++++++++++++++app.label:"+app!!.name+"\n\n\n")
-            print("+-------------------------r1.activityinfo.loadlabel: "+r1.loadLabel(manager)+"\n\n\n\n")
 
             app!!.icon = r1.loadIcon(manager)
-            print("++++++++++++++++++++++++++++++++app.icon:"+app!!.icon+"\n\n\n")
-            print("+-------------------------r1.activityinfo.loadicon: "+r1.loadIcon(manager)+"\n\n\n\n")
 
-            print("++++++++++++++++++++++++++++++++app: "+app+"\n\n\n\n")
-            Apps!!.add(app)
+                Apps!!.add(app)
+
+
 
         }
+
+    }
+
+    private fun FindApps(find:String){
+
+        manager=packageManager
+        Apps=ArrayList<Item>()
+        val intent = Intent(Intent.ACTION_MAIN,null)
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+
+        var availableActivities:List<ResolveInfo> = manager!!.queryIntentActivities(intent,0) /**Toma apps del telefono*/
+        val sortedAppsList = availableActivities.sortedBy { it.loadLabel(manager)?.toString() }
+        availableActivities = sortedAppsList
+
+        /**-------------------------Guardar Datos de las apps en una lista-----------------*/
+        for (r1 in availableActivities){
+            var app:Item = Item()
+            app!!.label=r1.activityInfo.packageName
+
+            app!!.name=r1.loadLabel(manager)
+
+            app!!.icon = r1.loadIcon(manager)
+            
+            if(find.equals("")){
+                Apps!!.add(app)
+            }else{
+                var NameAplication=app.name.toString().split(" ")
+
+                if(find.contains(" ")){
+                    if(NameAplication.size<=2){
+                        if(app.name.toString().startsWith(find,ignoreCase =true)){
+                            Apps!!.add(app)
+                        }
+                    }else{
+
+                        var sizeSearched=find.split(" ").size
+
+
+                        for (num in 0..NameAplication.size){
+                            if(num<(NameAplication.size-1)){
+                                var index_next_word=num+1
+                                var new_word=""
+
+                                for(n2 in 0..sizeSearched){
+                                    if(n2<num){
+                                        if(n2==0){
+                                            new_word=NameAplication[num+n2]
+                                        }else{
+                                            new_word=new_word+" "+NameAplication[num+n2]
+                                        }
+
+                                        print("-------------------------------------------New Word:"+new_word)
+                                    }
+
+
+                                }
+
+                                if(new_word.startsWith(find,ignoreCase =true)){
+                                    Apps!!.add(app)
+                                }
+
+                            }
+
+
+
+                        }
+                    }
+                }else{
+
+
+                        for (part_name in NameAplication){
+                            if(part_name.startsWith(find,ignoreCase =true)){
+                                Apps!!.add(app)
+                            }
+                        }
+
+
+
+
+                }
+
+
+            }
+
+
+        }
+
     }
 
     /**----------------------------------Metodo cargar apps en la Lista----------*/
@@ -67,7 +150,16 @@ class AppsList_Activity : AppCompatActivity() {
         AppsListView=findViewById(R.id.ListAppDrawer) as GridView
 
 
-        var adapter=CustomAdapter(applicationContext,Apps)
+        AppsListView!!.adapter=CustomAdapter(applicationContext, Apps) /**Adapter Personalizado CustomAdapter*/
+
+
+    }
+    private fun LoadAppsFinded(){
+
+
+        AppsListView=findViewById(R.id.ListAppDrawer) as GridView
+
+
         AppsListView!!.adapter=CustomAdapter(applicationContext, Apps) /**Adapter Personalizado CustomAdapter*/
 
 
@@ -78,11 +170,34 @@ class AppsList_Activity : AppCompatActivity() {
         AppsListView!!.setOnItemClickListener{Adapter: AdapterView<*>?, view: View?, position: Int, id: Long ->
             var intent:Intent =manager!!.getLaunchIntentForPackage(Apps!!.get(position).label.toString())
             startActivity(intent)
+
         }
 
 
 
     }
+
+    private fun OntextChange() {
+        searchAppsView=findViewById(R.id.SearchApps) as SearchView
+
+        searchAppsView!!.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                FindApps(newText)
+                LoadAppsFinded()
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // task HERE
+                print(" =========================Query:"+query)
+                return true
+            }
+
+        })
+    }
+
+
 
 
 }
